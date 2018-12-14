b05c5b02ee809c48c9da10e54767e0bd718e4742
Marta Hendel <marta.hendel@gmail.com>
2018-12-14 18:07:07 +0000

    Added first components

---
+++ src/Books.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SearchArea from "./SearchArea";
+
+class Books extends Component {
+  render() {
+    return (
+      <div>
+        <SearchArea />
+      </div>
+    );
+  }
+}
+
+export default Books;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <i className="fas fa-book fa-2x" />
+      <h1>Book Worm</h1>
+      <i className="fas fa-book fa-2x" />
+    </header>
+  );
+};
+
+export default Header;
