ec13ec0a9ad5b9c7292ecd65674e9f6f28ebc7d2
Marta Hendel <marta.hendel@gmail.com>
2018-12-18 20:58:09 +0000

    Added BookCard component

--- src/BookCard.js
+++ src/BookCard.js
@@ -1,7 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const BookCard = ({ book }) => <div />;
+const BookCard = props => {
+  return (
+    <div className="book__card-container">
+      <img src={props.image} alt="" />
+      <div className="book__card-info">
+        <h2>{props.title}</h2>
+        <h3>Author: {props.authors}</h3>
+        <h3>Publisher: {props.publisher}</h3>
+      </div>
+    </div>
+  );
+};
 
 export default BookCard;
 
--- src/BookList.js
+++ src/BookList.js
@@ -5,14 +5,17 @@ const BookList = props => {
   console.log(props);
   return (
     <div className="book__list">
-      {props.books.map(book => (
-        <div className="book__card" key={book.id}>
-          {console.log(book)}
-          <h1>{book.volumeInfo.title}</h1>
-          <h3>{book.volumeInfo.authors}</h3>
-          <h3>{book.volumeInfo.publisher}</h3>
-        </div>
-      ))}
+      {props.books.map(book => {
+        return (
+          <BookCard
+            key={book.id}
+            image={book.volumeInfo.imageLinks.thumbnail}
+            title={book.volumeInfo.title}
+            authors={book.volumeInfo.authors}
+            publisher={book.volumeInfo.publisher}
+          />
+        );
+      })}
     </div>
   );
 };
