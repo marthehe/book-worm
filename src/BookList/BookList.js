68e4ae5c7f58d8ff2f679da0279d6e3576f48cd3
Marta Hendel <marta.hendel@gmail.com>
2019-01-13 19:45:54 +0000

    Replace thumbnail

--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -4,13 +4,17 @@ import BookCard from "../BookCard/";
 const BookList = props => (
   <div className="book-list row">
     {props.books.map(book => {
+      let image;
+      if (book.volumeInfo.imageLinks) {
+        const thumbnail = book.volumeInfo.imageLinks.smallThumbnail;
+        image = thumbnail.replace("zoom=5", "zoom=3");
+      }
+
       return (
         <div className="col-12 col-md-6 col-xl-4 mb-4">
           <BookCard
             key={book.id}
-            image={
-              book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail
-            }
+            image={image}
             title={book.volumeInfo.title}
             authors={book.volumeInfo.authors}
             publisher={book.volumeInfo.publisher}
