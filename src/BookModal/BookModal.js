fdf5c7b2d00cdbea8c4a2c82c753f8aa526b2124
Marta Hendel <marta.hendel@gmail.com>
2019-01-13 21:07:05 +0000

    Fix modal style

--- src/BookModal/BookModal.js
+++ src/BookModal/BookModal.js
@@ -1,37 +1,38 @@
 import React from "react";
 
-const BookPage = props => {
+const BookModal = props => {
   return (
-    <div className="book-page container">
+    <div className="book-modal container">
       <button
         data-test="close-button"
-        className="book-list__card-close-button btn btn-primary"
+        className="book-modal__close-button btn btn-primary"
         data-dismiss="modal"
         onClick={props.handleCloseModal}
       >
         Close
       </button>
-      <div className="modal-header">
-        <h2 className="book-page__title modal-title">{props.title}</h2>
-      </div>
-      <div className="modal-body">
-        <div className="row">
-          <div className="col-sm-12 col-xl-6">
-            <img className="book-page__image" src={props.image} alt="" />
-          </div>
-          <div className="col-sm-12 col-xl-6">
-            <div className="book-page__info-container">
-              <div className="book-page__info">
-                <p className="book-page__subtitle">Author: {props.authors}</p>
-                <p className="book-page__subtitle">
-                  Publisher: {props.publisher}
-                </p>
-                <p className="book-page__subtitle">
-                  Published Date: {props.publishedDate}
-                </p>
-              </div>
-              <p className="book-page__description">{props.description}</p>
+
+      <h2 className="book-modal__title modal-title border-bottom">
+        {props.title}
+      </h2>
+
+      <div className="row">
+        <div className="col-sm-12 col-xl-6">
+          <img className="book-modal__image" src={props.image} alt="" />
+        </div>
+        <div className="col-sm-12 col-xl-6">
+          <div className="book-modal__info-container">
+            <div className="book-modal__info">
+              <p className="book-modal__subtitle small">
+                {Array.isArray(props.authors) &&
+                  `Author: ${props.authors.join(", ")}`}
+                <br />
+                Publisher: {props.publisher}
+                <br />
+                Published Date: {props.publishedDate}
+              </p>
             </div>
+            <p className="book-modal__description">{props.description}</p>
           </div>
         </div>
       </div>
@@ -39,4 +40,4 @@ const BookPage = props => {
   );
 };
 
-export default BookPage;
+export default BookModal;
